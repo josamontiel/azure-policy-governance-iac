1165950e6137802807d5ce55c19c6c3e4326aaed
0x <0x@MacBookPro.lan>
2026-05-03 21:51:44 +0100

    test: pipeline run

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -28,3 +28,4 @@ module "assignment_security_baselines" {
   initiative_id              = "/providers/microsoft.management/managementgroups/mg-governance-lab-prod/providers/Microsoft.Authorization/policySetDefinitions/${module.initiative_security_baselines.name}"
   log_analytics_workspace_id = var.log_analytics_workspace_id
 }
+
